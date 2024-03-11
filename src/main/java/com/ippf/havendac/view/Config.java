2a332db737a2b54fede449235d9c33b26865c0f7
Ivan Pinheiro <Ivanppf1@gmail.com>
2024-03-11 16:20:51 -0300

    refatorado o menu principal, isolado o menu de escolha das entidades no método showMenuEntidades.

--- src/main/java/com/ippf/havendac/view/Config.java
+++ src/main/java/com/ippf/havendac/view/Config.java
@@ -14,54 +14,56 @@ public class Config implements CommandLineRunner {
     private Menu menu;
 
 
-    public String showMenuEntidades() {
-        String menuImovel = """
+    public void showMenuEntidades() {
+        boolean continua = true;
+        String menuEntidades = """
 
                 1 - Imóvel
                 2 - Cômodo
                 S - Sair
                 ->\s""";
-        myCustomIO.print(menuImovel);
-        return myCustomIO.read();
-    }
 
-    public void showMenuOpcoes() {
-        boolean continua2 = true;
-        while (continua2) {
-            String menuImovel = """
-
-                    1 - Cadastrar novo
-                    2 - Listar todos
-                    S - Voltar
-                    ->\s""";
-            myCustomIO.print(menuImovel);
-            String resp = myCustomIO.read();
-            switch (resp) {
-                case "1" -> myCustomIO.print(menu.save());
-                case "2" -> menu.getAll().forEach((item) -> myCustomIO.print(item.toString() + "\n"));
-                case "S" -> continua2 = false;
-            }
-        }
-    }
-
-    @Override
-    public void run(String... args) {
-        boolean continua = true;
         while (continua) {
             try {
-                String resp = showMenuEntidades();
-                if (resp.equals("S")) {
-                    continua = false;
-                } else {
-                    switch (resp) {
-                        case "1" -> menu = menuImovel;
-                        case "2" -> menu = menuComodo;
-                    }
+                myCustomIO.print(menuEntidades);
+                String resp = myCustomIO.read();
+                switch (resp) {
+                    case "1" -> menu = menuImovel;
+                    case "2" -> menu = menuComodo;
+                    case "S" -> continua = false;
+                    default -> throw new RuntimeException("Opção inválida");
+                }
+                if (!resp.equals("S")) {
                     showMenuOpcoes();
                 }
             } catch (Exception e) {
                 myCustomIO.print(e.getMessage());
             }
         }
+    }
+
+    public void showMenuOpcoes() {
+        boolean continua = true;
+        String menuImovel = """
+
+                1 - Cadastrar novo
+                2 - Listar todos
+                S - Voltar
+                ->\s""";
+        while (continua) {
+            myCustomIO.print(menuImovel);
+            String resp = myCustomIO.read();
+            switch (resp) {
+                case "1" -> myCustomIO.print(menu.save());
+                case "2" -> menu.getAll().forEach((item) -> myCustomIO.print(item.toString() + "\n"));
+                case "S" -> continua = false;
+                default -> throw new RuntimeException("Opção inválida");
+            }
+        }
+    }
+
+    @Override
+    public void run(String... args) {
+        showMenuEntidades();
     }
 }
